d8ab02af27c0c67885bd7cb44f49dd3898165a7c
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-11-14 00:13:43 +0600

    How to copy a String

--- programming.c
+++ programming.c
@@ -1,2 +1,101 @@
+// #include <stdio.h>
+
+// int main()
+
+// {
+//     char str[20] = "abcde 5 6 2 ?";
+
+//     int length = 0;
+//     for (int i = 0; str[i] != '\0'; i++)
+//     {
+//         length++;
+//     }
+
+//     printf("%d", length);
+// }
+// #include <stdio.h>
+// #include<string.h>
+
+// int main()
+
+// {
+//     char str[20] = "Osman?";
+
+
+//     int length = strlen(str);
+
+//     printf("%d", length);
+// }
+// #include <stdio.h>
+// #include<string.h>
+
+// int main()
+
+// {
+//     char str[20];
+
+//     scanf("%s", str);
+
+//     int length = strlen(str);
+
+//     printf("%d", length);
+// }
+// #include <stdio.h>
+// #include<string.h>
+
+// int main()
+
+// {
+//     char a[20] = "string =;";
+
+//     char b[10] = "abcde";
+
+//     int length = strlen(b);
+
+//     printf("%s %s\n", a, b);
+
+//     for(int i = 0; i < length; i++){
+//         a[i] = b[i];
+//     }
+
+//     a[length] = '\0';
+
+//     printf("%s %s", a, b);
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// #include<string.h>
+
+// int main()
+// {
+//     char a[10] = "Arman";
+
+//     char b[15] = "Osman";
+
+//     printf("%s %s \n", a, b);
+
+//     strcpy(b, a);
+
+//     printf("%s %s \n", a, b);
+
+//     return 0;
+// }
+
 #include<stdio.h>
-//input test
+#include<string.h>
+
+int main()
+{
+    char a[10] = "Arman";
+
+    char b[15];
+
+
+    strcpy(b, "Osman");
+
+    printf("%s \n", b);
+
+    return 0;
+}
